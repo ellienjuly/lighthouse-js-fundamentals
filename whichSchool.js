0291dc243ae13679e5fd9f16985d85aec31d0634
Taeyeong Nam <ellienjuly@gmail.com>
2021-06-25 17:06:28 +0000

    Which school should you go?

---
+++ whichSchool.js
@@ -0,0 +1,12 @@
+// console.log("I am 35. Which school should I go to?");
+// console.log(whichSchool(35));
+// console.log("I am 8. Which school should I go to?");
+// console.log(whichSchool(8));
+// console.log("I am 14. Which school should I go to?");
+// console.log(whichSchool(14));
+
+const whichSchool = function (age) {
+  console.log(`I am ${age}. Which school should I go to?`);
+}
+
+console.log(whichSchool(35));
